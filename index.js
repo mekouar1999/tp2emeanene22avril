d93d339d7f56a75b9a93a65f2ce5c5c28b2f0e7a
mekouar1999 <othman.mekouar99@gmail.com>
2026-04-22 15:46:05 +0100

    navabr mobile ok

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, othman!")
+const toggle = document.getElementById('toggle');
+console.log(toggle);
+
+const navbar = document.getElementById('navbar');
+
+
+toggle.addEventListener('click', () => {
+    navbar.classList.toggle("othman")
+})
+
